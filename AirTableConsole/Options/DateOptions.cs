9b6c6d019218ed4891b00a1e925b1f2ae265971f
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-07-06 11:38:24 -0400

    Add comment warning not to choose more than 100 months at a time

--- AirTableConsole/Options/DateOptions.cs
+++ AirTableConsole/Options/DateOptions.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace AirTableConsole.Options {
+    /// <summary>
+    /// Stores First Date & Last Date, both Non-inclusive. For the purposes of this app,
+    /// there should be fewer than 100 months between the two dates (AirTable doesn't support
+    /// retrieving more than 100 records at a time)
+    /// </summary>
     public class DateOptions {
         public DateTime FirstDateNonInclusive { get; set; }
         public DateTime LastDateNonInclusive { get; set; }
